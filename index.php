0674d8d03cd68f6b3268db921c942bdff2b2f180
ShkuropatovSergey/site1 <axrro@mail.ru>
2021-02-03 08:06:50 +0300

    's'

--- index.php
+++ index.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-<<<<<<< HEAD
     <title>Document №3</title>
-=======
-    <title>Document #3</title>
->>>>>>> main-p
 </head>
 <body>
 <p>aaaaaaaaaaaaaa</p>
